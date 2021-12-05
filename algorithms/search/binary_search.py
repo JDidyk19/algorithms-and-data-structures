03e537cdbd428c29bba92def8e68083d3e30529f
Yurii Didyk <juran2201@gmail.com>
2021-12-05 17:00:39 +0200

    update binary_search, add tests for binary_search

--- algorithms/search/binary_search.py
+++ algorithms/search/binary_search.py
@@ -1,44 +1,39 @@
 
-# Iterative Binary Search Function
-def binary_search_iterative(element, array):
-    """TODO: Iterative search a given element in array[]
 
-    Time Complexity: O(log(n))
-    Space Complexity: O(1)
+def binary_search_iterative(array, element):
+    """Iterative search a given element in array[]
+
+    Time Complexity:    O(log(n))
+    Space Complexity:   O(1)
     """
     left = 0
     right = len(array) - 1
-
-    while left < right:
+    while left <= right:
         mid = (left + right) // 2
-        current = array[mid]
-        if current == element:
+        if array[mid] == element:
             return mid
-
-        elif current < element:
+        elif array[mid] < element:
             left = mid + 1
-
-        elif current > element:
+        elif array[mid] > element:
             right = mid - 1
     return -1
 
 
-# Recursive Binary Search Function
-def binary_search_recursive(element, array, left, right):
-    """TODO: Recursive search a given element in array[]
+def binary_search_recursive(array, element):
+    """Recursive search a given element in array[]
 
-        Time Complexity: O(log(n))
-        Space Complexity: O(log(n))
-        """
-    mid = (left + right) // 2
-    current = array[mid]
-    if left <= right:
-        if current == element:
-            return mid
+    Time Complexity:    O(log(n))
+    Space Complexity:   O(log(n))
+    """
+    def binary_search_recursive_(array, element, left, right):
+        if left <= right:
+            mid = (left + right) // 2
+            if array[mid] == element:
+                return mid
+            elif array[mid] < element:
+                return binary_search_recursive_(array, element, mid + 1, right)
+            elif array[mid] > element:
+                return binary_search_recursive_(array, element, left, mid - 1)
+        return -1
 
-        elif current < element:
-            return binary_search_recursive(element, array, mid + 1, right)
-
-        elif current > element:
-            return binary_search_recursive(element, array, left, mid - 1)
-    return -1
+    return binary_search_recursive_(array, element, 0, len(array) - 1)
---
+++ tests/test_binary_search.py
@@ -0,0 +1,36 @@
+import unittest
+from algorithms.search.binary_search import binary_search_iterative, binary_search_recursive
+
+
+class BinarySearchTest(unittest.TestCase):
+    def test_first_element(self):
+        array = list(range(1000))
+        self.assertEqual(binary_search_iterative(array, 0), 0)
+        self.assertEqual(binary_search_recursive(array, 0), 0)
+
+    def test_middle_element(self):
+        array = list(range(1000))
+        self.assertEqual(binary_search_iterative(array, 123), 123)
+        self.assertEqual(binary_search_recursive(array, 123), 123)
+
+    def test_last_element(self):
+        array = list(range(1000))
+        self.assertEqual(binary_search_iterative(array, 999), 999)
+        self.assertEqual(binary_search_recursive(array, 999), 999)
+
+    def test_empty_list(self):
+        array = list()
+        self.assertEqual(binary_search_iterative(array, 100), -1)
+        self.assertEqual(binary_search_recursive(array, 100), -1)
+
+    def test_element_not_in_list(self):
+        array = list(range(1000))
+        self.assertEqual(binary_search_iterative(array, 1000), -1)
+        self.assertEqual(binary_search_recursive(array, 1000), -1)
+
+    def test_random_elements(self):
+        array = [14, 23, 121, 146, 159, 328, 361, 388, 606, 628, 637, 668, 703, 704, 729, 729, 816, 878, 889, 976]
+        self.assertEqual(binary_search_iterative(array, 628), 9)
+        self.assertEqual(binary_search_recursive(array, 628), 9)
+
+
