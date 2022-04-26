37c22dbe1b06c45eed4d028df4d71cd9952e1828
Yurii Didyk <juran2201@gmail.com>
2022-04-26 17:49:41 +0300

    add hashmap

---
+++ data_structures/hashmap.py
@@ -0,0 +1,51 @@
+class HashMap:
+    def __init__(self, size=10):
+        self.size = size
+        self.buckets = [[] for _ in range(self.size)]
+
+    def __get_hash(self, key):
+        return hash(key) % self.size
+
+    def __getitem__(self, key):
+        hashed_key = self.__get_hash(key)
+        bucket = self.buckets[hashed_key]
+        for record in bucket:
+            record_key, record_value = record
+            if record_key == key:
+                return record_value
+
+    def __setitem__(self, key, val):
+        hashed_key = self.__get_hash(key)
+        bucket = self.buckets[hashed_key]
+        if bucket:
+            found_key = False
+            for idx, record in enumerate(bucket):
+                record_key, record_value = record
+                if record_key == key:
+                    found_key = True
+                    break
+
+            if found_key:
+                bucket[idx][-1] = val
+            else:
+                bucket.append((key, val))
+
+        else:
+            bucket = [(key, val)]
+            self.buckets[hashed_key] = bucket
+        return
+
+    def __delitem__(self, key):
+        hashed_key = self.__get_hash(key)
+        bucket = self.buckets[hashed_key]
+        for idx, record in enumerate(bucket):
+            record_key, record_value = record
+            if record_key == key:
+                bucket.pop(idx)
+                return
+
+    def __str__(self):
+        string = ''
+        for bucket in self.buckets:
+            string += f'{bucket}, '
+        return string.rstrip(', ')
