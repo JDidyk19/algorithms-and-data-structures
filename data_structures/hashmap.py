dfcdc66ca8f6dabeb118b3062105980e6dc88522
Yurii Didyk <juran2201@gmail.com>
2022-04-28 13:21:44 +0300

    hashmap with Linked list

--- data_structures/hashmap.py
+++ data_structures/hashmap.py
@@ -1,7 +1,10 @@
+from data_structures.linked_list import LinkedList
+
+
 class HashMap:
     def __init__(self, size=100):
         self.size = size
-        self.buckets = [[] for _ in range(self.size)]
+        self.buckets = [LinkedList() for _ in range(self.size)]
 
     def __get_hash(self, key):
         return hash(key) % self.size
@@ -9,39 +12,9 @@ class HashMap:
     def __getitem__(self, key):
         hashed_key = self.__get_hash(key)
         bucket = self.buckets[hashed_key]
-        for record in bucket:
-            record_key, record_value = record
-            if record_key == key:
-                return record_value
+        return bucket
 
     def __setitem__(self, key, val):
         hashed_key = self.__get_hash(key)
         bucket = self.buckets[hashed_key]
-        if bucket:
-            found_key = False
-            for idx, record in enumerate(bucket):
-                record_key, record_value = record
-                if record_key == key:
-                    found_key = True
-                    break
-
-            if found_key:
-                bucket[idx][-1] = val
-            else:
-                bucket.append((key, val))
-
-        else:
-            bucket.append((key, val))
-        return
-
-    def __delitem__(self, key):
-        hashed_key = self.__get_hash(key)
-        bucket = self.buckets[hashed_key]
-        for idx, record in enumerate(bucket):
-            record_key, record_value = record
-            if record_key == key:
-                bucket.pop(idx)
-                return
-
-    def __str__(self):
-        return ', '.join([str(bucket) for bucket in self.buckets])
+        bucket.push_back((key, val))
--- tests/data_structures/test_hashmap.py
+++ tests/data_structures/test_hashmap.py
@@ -2,6 +2,27 @@ import unittest
 from data_structures.hashmap import HashMap
 
 
+def show_bucket(head):
+    '''Converted a linked list to a list'''
+    values = list()
+    while head:
+        values.append(head.value)
+        head = head.next
+
+    return values
+
+def show_hashmap(hashmap):
+    '''Converted a hashmap to a list of buckets'''
+    buckets = list()
+    for bucket in hashmap.buckets:
+        if bucket.size == 0:
+            buckets.append(None)
+        else:
+            buckets.append(show_bucket(bucket.head))
+
+    return buckets
+
+
 class HashMapTest(unittest.TestCase):
 
     def test_size_hashmap(self):
@@ -10,46 +31,31 @@ class HashMapTest(unittest.TestCase):
 
     def test_empty_hashmap(self):
         hashmap = HashMap(5)
-        self.assertEqual(str(hashmap), '[], [], [], [], []')
+        self.assertEqual(show_hashmap(hashmap), [None, None, None, None, None])
 
     def test_set_key(self):
         hashmap = HashMap(5)
         hashmap[1] = 'value_1'
-        self.assertEqual(str(hashmap), "[], [(1, 'value_1')], [], [], []")
+        self.assertEqual(show_hashmap(hashmap), [None, [(1, 'value_1')], None, None, None])
 
     def test_get_value(self):
         hashmap = HashMap(5)
         hashmap[1] = 'value_1'
         value = hashmap[1]
-        self.assertEqual(value, 'value_1')
+        self.assertEqual(show_bucket(value.head), [(1, 'value_1')])
 
     def test_set_same_hash(self):
         hashmap = HashMap(5)
         hashmap[1] = 'value_1'
-        hashmap[11] = 'value_2'
-        self.assertEqual(str(hashmap), "[], [(1, 'value_1'), (11, 'value_2')], [], [], []")
+        hashmap[11] = 'value_11'
+        self.assertEqual(show_hashmap(hashmap), [None, [(1, 'value_1'), (11, 'value_11')], None, None, None])
 
     def test_get_same_hash(self):
         hashmap = HashMap(5)
         hashmap[1] = 'value_1'
-        hashmap[11] = 'value_2'
+        hashmap[11] = 'value_11'
         value = hashmap[11]
-        self.assertEqual(value, 'value_2')
-
-    def test_del_key(self):
-        hashmap = HashMap(5)
-        hashmap[1] = 'value_1'
-        self.assertEqual(str(hashmap), "[], [(1, 'value_1')], [], [], []")
-        del hashmap[1]
-        self.assertEqual(str(hashmap), '[], [], [], [], []')
-
-    def test_del_key_same_hash(self):
-        hashmap = HashMap(5)
-        hashmap[1] = 'value_1'
-        hashmap[11] = 'value_2'
-        self.assertEqual(str(hashmap), "[], [(1, 'value_1'), (11, 'value_2')], [], [], []")
-        del hashmap[1]
-        self.assertEqual(str(hashmap), "[], [(11, 'value_2')], [], [], []")
+        self.assertEqual(show_bucket(value.head), [(1, 'value_1'), (11, 'value_11')])
 
 
 if __name__ == '__main__':
